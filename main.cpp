07243e86db983a9190708620c406cc340a5a9865
yxl20190811 <53991020+yxl20190811@users.noreply.github.com>
2023-03-18 08:50:16 +0800

    Update main.cpp
    增加了10进制，18进制，8进制，2进制的字符串与大整数的互转函数

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-﻿// BigInt.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// BigInt.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
@@ -6,23 +6,22 @@
 
 int main()
 {
-    const char* str = "12345678901234567890123456789012345678901234567890";
-    TBigInt::IntType  BigIntVect[1000];
-    int BigIntSize = sizeof(BigIntVect)/sizeof(TBigInt::IntType);
-    TBigInt::Text2Uint(str, strlen(str), BigIntVect, BigIntSize);
+    char buf[10240];
+    TBigUint  i;
+    i.BinText2This("111010100001110001110");
+    i.toBinText(buf, sizeof(buf));
 
-    char text[1000];
-    int size = sizeof(text) / sizeof(char);
-    TBigInt::Uint2Text(BigIntVect, BigIntSize, text, size);
+    i.toOctText(buf, sizeof(buf));
+    i.OctText2This(buf);
+
+    i.toHexText(buf, sizeof(buf));
+    i.HexText2This(buf);
+
+    const char* t = "99999999999999999999999999999999999999999999999999999999";
+    i.DecText2This(t);
+    i.toDecText(buf, sizeof(buf));
+    if (0 != strcmp(t, buf)) {
+        abort();
+    }
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
